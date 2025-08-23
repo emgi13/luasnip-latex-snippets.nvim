e0c862d43c623b5e592682e2b0fda1c6c01d3b86
emgixiii <emgi@live.in>
2025-08-24 02:43:13 +0530

    feat: better statistics autocomplete

--- lua/luasnip-latex-snippets/math_wA_no_backslash.lua
+++ lua/luasnip-latex-snippets/math_wA_no_backslash.lua
@@ -26,8 +26,9 @@ function M.retrieve(is_math)
     parse_snippet({ trig = "set", name = "set" }, [[ \\{$1\\} $0 ]]),
     parse_snippet({ trig = "fun", name = "function map" }, "f \\colon $1 \\R \\to \\R \\colon $0"),
     -- Probability
-    parse_snippet({ trig = "Prob", name = "Probability" }, "\\text{Pr}\\left[ $1 \\right] $0"),
-    parse_snippet({ trig = "Exp", name = "Expectation" }, "\\text{E}\\left[ $1 \\right] $0"),
+    parse_snippet({ trig = "Prob", name = "Probability" }, "\\mathbb{P}\\left( $1 \\right) $0"),
+    parse_snippet({ trig = "Exp", name = "Expectation" }, "\\mathbb{E}\\left[ $1 \\right] $0"),
+    parse_snippet({ trig = "Var", name = "Variance" }, "\\mathrm{Var}\\left[ $1 \\right] $0"),
 
     parse_snippet(
       { trig = "abs", name = "absolute value \\abs{}" },
